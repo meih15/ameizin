4aef8ff0248925782e5d8aadffb89d0db497af30
meih15 <meihuang8328@gmail.com>
2023-02-03 16:45:05 -0500

    profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from "./components/SignUpFormPage";
+import Navigation from "./components/Navigation";
+
 
 function App() {
   return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup">
-        <SignupFormPage />
-      </Route>
-    </Switch>
+    <>
+    <Navigation />
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
