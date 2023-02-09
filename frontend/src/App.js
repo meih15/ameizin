8b2881104746d21acdcf5964a9cb0c20c24d35fc
meih15 <meihuang8328@gmail.com>
2023-02-08 21:56:23 -0500

    page not found component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from "./components/SignUpFormPage";
 import Header from "./components/Header";
-// import PageNotFound from './components/PageNotFound';
+import PageNotFound from './components/PageNotFound';
 import ProductShowPage from './components/ProductShowPage';
 // import Navigation from "./components/Navigation";
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/signup"><SignupFormPage /></Route>
         <Route path='/products/:productId'><ProductShowPage /></Route> 
         <Route exact path='/'><Header /></Route>
-        {/* <Route path="*"><PageNotFound /></Route> */}
+        <Route path="*"><PageNotFound /></Route>
       </Switch>
     </>
   );
