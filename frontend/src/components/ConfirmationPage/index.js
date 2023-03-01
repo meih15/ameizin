e0b7a29bc24049205ec8b08c2b810331ff462658
meihuang <meihuang8328@gmail.com>
2023-03-01 16:22:58 -0500

    confirmation page styling

--- frontend/src/components/ConfirmationPage/index.js
+++ frontend/src/components/ConfirmationPage/index.js
@@ -35,7 +35,7 @@ const ConfirmationPage = () => {
                 </div>
     });
 
-    const placedProduct = confirmedList.first;
+    const placedProduct = filteredConfirmedItems[0];
 
     const handleContinueShoppingClick = e => {
         e.preventDefault();
@@ -48,39 +48,72 @@ const ConfirmationPage = () => {
                 <Header />
                 <CategoryNavBar />
             </div>
-            <div id='confirmation-section'>
-                <div id='order-placed-check'>
-                    <i id='order-check' className="fa-solid fa-circle-check" />
-                    <h1 id='thanks-message'>Order placed, thanks!</h1>
-                </div>
-                <div id='confirm-total-price'>
-                    <div className='order-number-section'>
-                        <h2 id='order-number-text'>Order:</h2>
-                        <h3>#{placedProduct.orderConfirmation}</h3>
+            <div className='center-pls'>
+                <div id='confirmation-section'>
+                    <div id='order-placed-check'>
+                        <i id='order-check' className="fa-solid fa-circle-check" />
+                        <h1 id='thanks-message'>Order placed, thanks!</h1>
                     </div>
-                    <div className='confirmed-order-total-section'>
-                        <div id='confirm-order-subtotal'>
-                            <h4 id='confirm-subtotal-text'>Sub-total</h4>
-                            {/* price */}
+                    <div id='thanks-break'/>
+                    <div id='confirm-total-price'>
+                        <div className='order-number-section'>
+                            <h2 id='order-number-text'>Order:</h2>
+                            <h3 id='order-number' >#{placedProduct?.orderConfirmation}</h3>
                         </div>
-                        <div id='tax-confirm-section'>
-                            <h5 id='tax-confirm-text'>+ Tax</h5>
-                            <h6 id='zero-tax'>$0.00</h6>
+                        <div className='confirmed-order-total-section'>
+                            <div id='confirm-order-subtotal'>
+                                <h4 id='confirm-subtotal-text'>Sub-total</h4>
+                                <div className='confirmed-subtotal-item-price'>
+                                    <p id='confirmed-subtotal-symbol'>$</p>
+                                    <p id='confirmed-subtotal-whole-number-info'>{(Math.floor(placedProduct?.orderTotal)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.</p>
+                                    <p id='confirmed-subtotal-cents-info'>{Math.floor((placedProduct?.orderTotal % 1) * 100) === 0 ? '00' : Math.floor((placedProduct?.orderTotal % 1) * 100)}</p>
+                                </div>
+                            </div>
+                            <div id='tax-confirm-section'>
+                                <h5 id='tax-confirm-text'>+ Tax</h5>
+                                <h6 id='zero-tax'>$0.00</h6>
+                            </div>
+                            <div id='total-confirm'>
+                                <h7 id='total-text-confirm'>Total</h7>
+                                <div className='confirmed-total-item-price'>
+                                    <p id='confirmed-total-symbol'>$</p>
+                                    <p id='confirmed-total-whole-number-info'>{(Math.floor(placedProduct?.orderTotal)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.</p>
+                                    <p id='confirmed-total-cents-info'>{Math.floor((placedProduct?.orderTotal % 1) * 100) === 0 ? '00' : Math.floor((placedProduct?.orderTotal % 1) * 100)}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div id='total-confirm'>
-                            <h7 id='total-text-confirm'>Total</h7>
-                            {/* price */}
+                    </div>
+                    <div id='thanks-break'/>
+                    <div className='ordered-products'>
+                        {confirmedList}
+                    </div>
+                    <div id='confirm-review'>
+                        <Link id='review-confirm' to='/orderhistory'>Review your orders</Link>
+                        <i id='greater-than' className="fa-solid fa-greater-than"></i>
+                    </div>
+                    <div id='thanks-break'/>
+                    <div id='confirm-button'>
+                        <button onClick={handleContinueShoppingClick} id='continue-shopping-button'>Continue shopping</button>
+                    </div>
+                    <div id='thanks-break'/>
+                    <div id='done-shopping-section'>
+                        <p className='done'>Done Shopping?</p>
+                        <p className='check-it'>Check out these Ameizin' links:</p>
+                        <div className='done-links'>
+                            <p id='portfolio-link'>Portfolio</p>
+                            <a id='github-link' href='https://github.com/meih15' >Github</a>
+                            <a id='linkedin-link' href='https://www.linkedin.com/in/mei-huang-ba967a159'>Linkedin</a>
+                            <div className='description-link'>
+                                <a id='js-link' href='https://meih15.github.io/bobabreak/'>bobabreak</a>
+                                <p id='des'>Javascript Project</p>
+                            </div>
+                            <div className='description-link'>
+                                <a id='m-link' href='https://puppy-play-dates.onrender.com/'>Puppy Play Dates</a>
+                                <p id='des'>MERN Stack Project</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='ordered-products'>
-                    {confirmedList}
-                </div>
-                <div>
-                    <Link to='/orderhistory'>Review your orders</Link>
-                    <i id='greater-than' className="fa-solid fa-greater-than"></i>
-                </div>
-                <button onClick={handleContinueShoppingClick} id='continue-shopping-button'>Continue shopping</button>
             </div>
         </div>
     )
