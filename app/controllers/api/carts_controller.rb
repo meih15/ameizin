82a692389b4a3866753648f32d8eb4512c4c7b64
meihuang <meihuang8328@gmail.com>
2023-03-22 22:54:23 -0400

    changed carts controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,56 +1,21 @@
 class Api::CartsController < ApplicationController
 
 
-    # def index
-    #     # @cart_items = current_user.cart
-    #     @cart_items = current_user.carts
-    #     render :index
-    # end
-
     def show
         if current_user
             @cart = current_user.cart
             render :show
         else
             if session[:cart] 
-                @cart = Cart.find_by(id: session[:cart])
+                @cart = Cart.find(session[:cart])
                 render :show
             else
-                @cart = Cart.create
+                @cart = Cart.create!
                 session[:cart] = @cart.id
                 render :show
             end
         end
     end
-
-    # def create
-    #     @cart_item = Cart.new(cart_params)
-    #     if @cart_item.save
-    #         render :show
-    #     else
-    #         render json: { errors: @cart_item.errors.full_messages }
-    #     end
-    # end
-
-    # def update
-    #     @cart_item = Cart.find_by(product_id: cart_params[:product_id], user_id: current_user.id)
-    #     if @cart_item
-    #         @cart_item.quantity = cart_params[:quantity].to_i
-    #         @cart_item.save
-    #         render :show
-    #     else
-    #         render json: {errors: 'Could not update quantity'}
-    #     end
-    # end
-
-    # def destroy
-    #     @cart_item = Cart.find_by(id: params[:id])
-    #     if @cart_item.destroy
-    #         render json: {}
-    #     end
-
-    # end
-
 
 
 end
